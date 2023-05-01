667326cec45adcc03fafb1b8994382a1afdd4d6f
mctseng <mctzeng@gmail.com>
2023-05-01 22:14:10 +0800

    commmit quick_start

--- quick_start.py
+++ quick_start.py
@@ -3,12 +3,11 @@ import os
 from mygetpass import get_file_contents
 
 
-
 os.environ["OPENAI_API_KEY"] = get_file_contents("openai_api_key.secret")
 #export OPENAI_API_KEY="..." in terminal
 
+def create_name() -> None:
+    llm = OpenAI(temperature=0.9)
+    text = "What would be a good company name for a company that makes AI youtube video?"
+    print(llm(text))
 
-
-llm = OpenAI(temperature=0.9)
-text = "What would be a good company name for a company that makes AI youtube video?"
-print(llm(text))
